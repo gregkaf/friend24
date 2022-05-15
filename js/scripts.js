5205c8954cc51887a2407cc9747b3d423bca16df
gregorykafanov <gregkaf@gmail.com>
2022-05-15 13:32:12 +0300

    +

--- js/scripts.js
+++ js/scripts.js
@@ -7516,20 +7516,20 @@
 });
 
 // index
-$(document).ready(function () {
-  $('.center-slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: true,
-    arrows: true,
-    dots: true,
-    speed: 300,
-    centerPadding: '20px',
-    infinite: true,
-    autoplaySpeed: 5000,
-    autoplay: false,
-  });
-});
+// $(document).ready(function () {
+//   $('.center-slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     centerMode: true,
+//     arrows: true,
+//     dots: true,
+//     speed: 300,
+//     centerPadding: '20px',
+//     infinite: true,
+//     autoplaySpeed: 5000,
+//     autoplay: false,
+//   });
+// });
 
 const fbPixelsEvent = () => fbq('trackCustom', 'Add to Wishlist');
 
---
+++ js/slick.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.slider24').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    arrows: true,
+    dots: true,
+    speed: 300,
+    centerPadding: '20px',
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: false,
+  });
+});
